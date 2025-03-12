c42e06953f93adc41619883efb6d741777e1d7e0
Danish Craft <121832703+DanishCraftYT@users.noreply.github.com>
2025-03-12 01:46:19 +0100

    moved to vcpkg instead of msys2. added a "InstallLibraries.py" script which installs GLFW. updated .gitignore file. updated the README.md file.

---
+++ InstallLibraries.py
@@ -0,0 +1,58 @@
+import shutil
+import os
+
+# asks user for their vcpkg directory.
+vcpkg_dir = input("path to VCPKG installation: ")
+
+# exits the program if the vcpkg directory is invalid.
+if not os.path.exists(os.path.join(vcpkg_dir, "vcpkg.exe")):
+    print("please specify a valid vcpkg directory")
+    input("press enter to exit...")
+    exit()
+
+# contains the path to the GLFW package.
+glfw_path = os.path.join(vcpkg_dir, "packages/glfw3_x64-mingw-dynamic")
+
+# installs the GLFW3 vcpkg package if it's not already installed.
+if not os.path.exists(glfw_path):
+    print("installing GLFW3 vcpkg package")
+    os.system("vcpkg install glfw3")
+
+# get's the directory of this script.
+user_dir = os.path.abspath(os.path.dirname(__file__))
+
+# creates the "lib" folder if it doesn't exist.
+if not os.path.exists(os.path.join(user_dir, "lib")):
+    print("creating lib folder")
+    os.mkdir(os.path.join(user_dir, "lib"))
+
+# copies the "glfw3.dll" file into the "lib" folder if it's not already in that folder.
+if not os.path.exists(os.path.join(user_dir, "lib/glfw3.dll")):
+    print("getting glfw3.dll")
+    shutil.copy(os.path.join(glfw_path, "bin/glfw3.dll"), os.path.join(user_dir, "lib/"))
+
+# copies the "libglfw3dll.a" file into the "lib" folder if it's not already in that folder.
+if not os.path.exists(os.path.join(user_dir, "lib/libglfw3dll.a")):
+    print("getting libglfw3dll.a")
+    shutil.copy(os.path.join(glfw_path, "lib/libglfw3dll.a"), os.path.join(user_dir, "lib/"))
+
+# copies the GLFW source code into the include folder if it's not alreadt in that folder.
+if not os.path.exists(os.path.join(user_dir, "include/GLFW/")):
+    print("getting GLFW source files")
+    if not os.path.exists(os.path.join(user_dir, "include/GLFW")):
+        os.mkdir(os.path.join(user_dir, "include/GLFW"))
+    glfw_source_path = os.path.join(glfw_path, "include/GLFW")
+    for filename in os.listdir(glfw_source_path):
+        shutil.copy(os.path.join(glfw_source_path, filename), os.path.join(user_dir, "include/GLFW/"))
+
+# exists the program if the build output folder doesn't exist.
+if not os.path.exists(os.path.join(user_dir, "out/build/CMakeOpenGLPreset")):
+    print("failed to find the build output folder. please run this script again once the build output folder has been created so it can copy the glfw3.dll file into the build output folder")
+    exit()
+
+# copies the "glfw3.dll" file into the build output folder if it isn't already in that folder.
+if not os.path.exists(os.path.join(user_dir, "out/build/CMakeOpenGLPreset/glfw3.dll")):
+    print("copying the glfw3.dll file to the build output folder")
+    shutil.copy(os.path.join(user_dir, "lib/glfw3.dll"), os.path.join(user_dir, "out/build/CMakeOpenGLPreset/"))
+
+print("installed all libraries")
--- README.md
+++ README.md
@@ -1,12 +1,17 @@
 # Overview
-uses CMake to build OpenGL, GLFW, Glad, and ImGUI. it's been tested using MinGW64 in Visual Studio Code on Windows. feel free to use my code or CMakelists.txt file in your
-project.<br>
+uses CMake to build OpenGL, GLFW, Glad, and ImGUI. it's been tested using the GCC compiler, vcpkg and Visual Studio Code. feel free to use my code or CMakelists.txt file in your project.<br>
 
 # Requirements
-* MinGW64 and GCC compiler.<br>
-* MinGW64 GLFW package. open `mingw64.exe` and use this command to install it: `pacman -S mingw-w64-x86_64-glfw`.<br>
+* GCC compiler.<br>
+* vcpkg glfw3 package. by default the glfw3 package files are included in this project however if you want to update them. you can follow [this guide]().<br>
 * Visual Studio Code.<br>
 * Visual Studio Code Extensions: C/C++ and CMake Tools.<br>
 
 # Building The Project
 once you have met all the requirements listed above. clone the repo and open it in Visual Studio Code. then in the CMake tab you can build the project.<br>
+
+## IMPORTANT
+if the "glfw3.dll" file is missing from the build output directory. please run the "InstallLibraries.py" script which will copy the "glfw3.dll" file into the build output directory or copy and paste the "glfw3.dll" file into the build output directory. the "glfw3.dll" file can be found in the "lib" folder.<br>
+
+# updating GLFW
+to update GLFW. first update or install the glfw3 package. then delete the "lib" and "include/GLFW" folders in this project. then delete the "glfw3.dll" file in the build output folder in this project. then run the "InstallLibraries.py" script to install the glfw3 package and copy it's files into this project. then build the project and run it.<br>
