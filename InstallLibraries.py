eb8c525b06eb7925792d690a2d5ec08e1b612610
Danish Craft <121832703+DanishCraftYT@users.noreply.github.com>
2025-05-04 15:35:41 +0100

    fixed a issue where the script would crash when trying to install the GLFW library in VCPKG. you can now choose if you want to update GLFW or not. updated to latest version of GLFW. added "run.bat" which runs the generated .exe file created by CMake. updated README.md file.

--- InstallLibraries.py
+++ InstallLibraries.py
@@ -1,44 +1,50 @@
-import subprocess
 import shutil
 import os
-
-# asks user for their vcpkg directory.
-vcpkg_dir = input("path to VCPKG installation: ")
-
-# exits the program if the vcpkg directory is invalid.
-if not os.path.exists(os.path.join(vcpkg_dir, "vcpkg.exe")):
-    print("please specify a valid vcpkg directory")
-    input("press enter to exit...")
-    exit()
-
-# contains the path to the GLFW package.
-glfw_path = os.path.join(vcpkg_dir, "packages/glfw3_x64-mingw-dynamic")
-
-# installs the GLFW3 vcpkg package if it's not already installed.
-if not os.path.exists(glfw_path):
-    print("installing GLFW3 vcpkg package")
-    subprocess.call("vcpkg install glfw3 --triplet=x64-mingw-dynamic --host-triplet=x64-mingw-dynamic")
 
 # get's the directory of this script.
 user_dir = os.path.abspath(os.path.dirname(__file__))
 
-# creates the "lib" folder if it doesn't exist.
-if not os.path.exists(os.path.join(user_dir, "lib")):
-    print("creating lib folder")
-    os.mkdir(os.path.join(user_dir, "lib"))
+update_GLFW = input("do you want to update GLFW? (y/n): ").lower()
 
-# copies the "glfw3.dll" file into the "lib" folder if it's not already in that folder.
-if not os.path.exists(os.path.join(user_dir, "lib/glfw3.dll")):
+if update_GLFW == "y":
+    # asks user for their vcpkg directory.
+    vcpkg_dir = input("path to VCPKG installation: ")
+
+    # exits the program if the vcpkg directory is invalid.
+    if not os.path.exists(os.path.join(vcpkg_dir, "vcpkg.exe")):
+        print("please specify a valid vcpkg directory")
+        input("press enter to exit...")
+        exit()
+
+    # creates the "lib" folder if it doesn't exist.
+    if not os.path.exists(os.path.join(user_dir, "lib")):
+        print("creating lib folder")
+        os.mkdir(os.path.join(user_dir, "lib"))
+    
+    # REINSTALLS GLFW3 PACKAGE FROM VCPKG #
+
+    # removes the GLFW3 package.
+    print("removing GLFW3 vcpkg package")
+    os.system("vcpkg remove glfw3 --triplet=x64-mingw-dynamic --host-triplet=x64-mingw-dynamic")
+
+    # installs the GLFW3 vcpkg package if it's not already installed.
+    print("installing GLFW3 vcpkg package")
+    os.system("vcpkg install glfw3 --triplet=x64-mingw-dynamic --host-triplet=x64-mingw-dynamic")
+    
+    # UPDATES GLFW #
+
+    # contains the path to the GLFW package.
+    glfw_path = os.path.join(vcpkg_dir, "packages/glfw3_x64-mingw-dynamic")
+    
+    # copies the "glfw3.dll" file into the "lib" folder if it's not already in that folder.
     print("getting glfw3.dll")
     shutil.copy(os.path.join(glfw_path, "bin/glfw3.dll"), os.path.join(user_dir, "lib/"))
 
-# copies the "libglfw3dll.a" file into the "lib" folder if it's not already in that folder.
-if not os.path.exists(os.path.join(user_dir, "lib/libglfw3dll.a")):
+    # copies the "libglfw3dll.a" file into the "lib" folder if it's not already in that folder.
     print("getting libglfw3dll.a")
     shutil.copy(os.path.join(glfw_path, "lib/libglfw3dll.a"), os.path.join(user_dir, "lib/"))
 
-# copies the GLFW source code into the include folder if it's not alreadt in that folder.
-if not os.path.exists(os.path.join(user_dir, "include/GLFW/")):
+    # copies the GLFW source code into the include folder if it's not alreadt in that folder.
     print("getting GLFW source files")
     if not os.path.exists(os.path.join(user_dir, "include/GLFW")):
         os.mkdir(os.path.join(user_dir, "include/GLFW"))
@@ -51,8 +57,8 @@ if not os.path.exists(os.path.join(user_dir, "build/")):
     print("failed to find the build output folder. please run this script again once the build output folder has been created so it can copy the glfw3.dll file into the build output folder")
     exit()
 
-# copies the "glfw3.dll" file into the build output folder if it isn't already in that folder.
-if not os.path.exists(os.path.join(user_dir, "build/glfw3.dll")):
+# copies the "glfw3.dll" file into the build output folder if it isn't already in that folder or GLFW was updated.
+if not os.path.exists(os.path.join(user_dir, "build/glfw3.dll")) or update_GLFW == "y":
     print("copying the glfw3.dll file to the build output folder")
     shutil.copy(os.path.join(user_dir, "lib/glfw3.dll"), os.path.join(user_dir, "build/"))
 
--- README.md
+++ README.md
@@ -3,13 +3,13 @@ uses CMake to build OpenGL, GLFW, Glad, and ImGUI. it's been tested using the GC
 
 # Requirements
 * GCC compiler.<br>
+* Python 3.13.<br>
+
+## OPTIONAL
 * vcpkg glfw3 package. by default the glfw3 package files are included in this project however if you want to update them. you can follow [this guide](https://github.com/DanishCraftYT/OpenGLCMake?tab=readme-ov-file#updating-glfw).<br>
 
 # Building The Project
-once you have met all the requirements listed above. clone the repo and open it in Visual Studio Code. then in the CMake tab you can build the project.<br>
-
-## IMPORTANT
-if the "glfw3.dll" file is missing from the build output directory. please run the "InstallLibraries.py" script which will copy the "glfw3.dll" file into the build output directory or copy and paste the "glfw3.dll" file into the build output directory. the "glfw3.dll" file can be found in the "lib" folder.<br>
+once you have met all the requirements listed above. clone the repo and run "WinBuild.py". then run the "InstallLibraries.py" file.<br>
 
 # updating GLFW
 to update GLFW. first update the glfw3 package. then delete the "lib" and "include/GLFW" folders in this project.
